3363f2cc3ce5f09c3c6fa5ae8df47f56fb2cfefe
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-06-04 13:28:19 +0000

    feat: Add RestaurantController to handle GET request for restaurants

---
+++ restaurant-listing/src/main/java/com/gesacademy/restaurantlisting/controller/RestaurantController.java
@@ -0,0 +1,28 @@
+package com.gesacademy.restaurantlisting.controller;
+
+import com.gesacademy.restaurantlisting.dto.RestaurantDto;
+import com.gesacademy.restaurantlisting.service.RestaurantService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RestaurantDto>> getAllRestaurants() {
+        List<RestaurantDto> restaurants = restaurantService.getAllRestaurants();
+        return new ResponseEntity<>(restaurants, HttpStatus.OK);
+    }
+}
